9760258419e4a7bf6ea690743e1c7dee30d0a2f6
risonakamo <hmykanae@gmail.com>
2018-03-19 14:21:31 -0400

    tried some more things

--- test2.r
+++ test2.r
@@ -1,12 +1,18 @@
 library(dplyr,warn.conflicts=FALSE);
 
 #testing how to init data frames and add things to them
+#need to keep track of max size of dataframe and current size and colnames
+#colnames can be gotten with function colnames
 bob<-data.frame(matrix(ncol=3,nrow=3));
 colnames(bob)<-c("hey","there","how");
 bobindex<-1;
+bobsize<-3;
 
-bob[bobindex,]<-c(1,2,3);
-bobindex=bobindex+1;
+bob[bobindex,]<-c(1,"aa",3);
+bobindex<-bobindex+1;
+
+bob[bobindex,]<-c(4,"he",3);
+bobindex<-bobindex+1;
 
 bob2<-data.frame(matrix(ncol=3,nrow=3));
 colnames(bob2)<-c("hey","there","how");
@@ -15,7 +21,9 @@ bob<-rbind(bob,bob2);
 print(bob);
 
 #how to printf...i feel like theres a better way
-cat(sprintf("%i\n",bob[1,1]));
+cat(sprintf("%s\n",bob[1,1]));
+
+print(colnames(bob));
 
 #so numbers can be used as bools
 testbool<-0;
