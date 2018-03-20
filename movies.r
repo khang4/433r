c7fb2309df81044df3af831d8186a2455bd74252
risonakamo <hmykanae@gmail.com>
2018-03-20 17:48:59 -0400

    scatter plot working probably

--- movies.r
+++ movies.r
@@ -7,22 +7,37 @@ failreasons<-function(alldata)
     relevantdata<-data.frame(count(select(alldata,clean_test),clean_test));
     # colnames(relevantdata)<-c("dubious","men","notalk","nowomen","ok");
     # relevantdata<-relevantdata[2,];
-    print(relevantdata);
+    # print(relevantdata);
 
     ggplot(data=relevantdata,aes(x=clean_test,y=n))+geom_bar(stat="identity");
     ggsave("graph1.png");
 }
 
+violin<-function(alldata)
+{
+    relevantdata<-select(alldata,binary,code);
+    relevantdata$code<-as.numeric(gsub("PASS|FAIL","",relevantdata$code));
+    # relevantdata$binary<-gsub("PASS",1,relevantdata$binary);
+    # relevantdata$binary<-gsub("FAIL",0,relevantdata$binary);
+
+    # relevantdata<-data.frame(count(relevantdata,code,binary));
+
+    # print(relevantdata);
+
+    ggplot(data=relevantdata,aes(binary,code))+geom_violin();
+    ggsave("graph2.png");
+}
+
+scattuh<-function(alldata)
+{
+    rdata<-select(alldata,clean_test,budget_2013.,domgross_2013.);
+    rdata$budget_2013.=strtoi(rdata$budget_2013.);
+    rdata$domgross_2013.=strtoi(rdata$domgross_2013.);
+    # print(rdata);
+    # print(alldata);
+
+    ggplot(data=rdata,aes(x=budget_2013.,y=domgross_2013.,colour=clean_test))+geom_point();
+    ggsave("graph4.png");
+}
+
 alldata<-read.csv(file="data/movies.csv",header=TRUE);
-
-relevantdata<-select(alldata,binary,code);
-relevantdata$code<-as.numeric(gsub("PASS|FAIL","",relevantdata$code));
-# relevantdata$binary<-gsub("PASS",1,relevantdata$binary);
-# relevantdata$binary<-gsub("FAIL",0,relevantdata$binary);
-
-# relevantdata<-data.frame(count(relevantdata,code,binary));
-
-print(relevantdata);
-
-ggplot(data=relevantdata,aes(binary,code))+geom_violin();
-ggsave("graph2.png");
