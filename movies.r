6bb0adfea4ac59bb02c1c9ef86d139e71ad7d165
risonakamo <hmykanae@gmail.com>
2018-03-20 13:46:07 -0400

    ggplot makes no sense

--- movies.r
+++ movies.r
@@ -1,7 +1,13 @@
 library(dplyr,warn.conflicts=FALSE);
+library(ggplot2,warn.conflicts=FALSE);
 options(width=2000);
 
 alldata<-read.csv(file="data/movies.csv",header=TRUE);
 
-relevantdata<-count(select(alldata,clean_test),clean_test);
+relevantdata<-data.frame(count(select(alldata,clean_test),clean_test));
+# colnames(relevantdata)<-c("dubious","men","notalk","nowomen","ok");
+# relevantdata<-relevantdata[2,];
 print(relevantdata);
+
+ggplot(data=relevantdata,aes(x=clean_test,y=n))+geom_bar(stat="identity");
+ggsave("graph.png");
