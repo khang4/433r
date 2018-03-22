5ba55321f6447e4e0247d4a830bae1aec315792d
risonakamo <hmykanae@gmail.com>
2018-03-22 17:08:44 -0400

    row names are colliding with words in text

--- words.r
+++ words.r
@@ -28,10 +28,10 @@ addword.ddict<-function(dict,word)
 
     if (dict[["index"]]>=dict[["size"]])
     {
-        expanddict<-data.frame(matrix(ncol=1,nrow=dict[["increment"]]),row.names=c(dict$size+1:dict$size+dict$increment));
+        expanddict<-data.frame(matrix(ncol=1,nrow=dict[["increment"]]),row.names=c((dict$size+1):(dict$size+dict$increment)));
         colnames(expanddict)<-c("count");
         dict[["dictdata"]]<-rbind(dict[["dictdata"]],expanddict);
-        dict$size<-dict$size+dict[["increment"]];
+        dict$size<-dict$size+dict$increment;
     }
 
     return(dict);
@@ -50,7 +50,7 @@ sortdict.ddict<-function(dict)
     return(dict);
 }
 
-text<-scan("data/hounds.txt",character());
+text<-scan("data/hounds.txt",character(),quote=NULL);
 
 letters<-0;
 
