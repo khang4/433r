98dc1de2ac877bbb1eaa34b31a5fbdb5f51115a2
risonakamo <hmykanae@gmail.com>
2018-03-29 21:02:46 -0400

    gathered required data

--- words.r
+++ words.r
@@ -5,14 +5,19 @@ options(width=2000);
 # text<-scan("data/victorious.txt",character(),quote=NULL,fileEncoding="UTF-8");
 invisible(text<-strsplit(read_file("data/hey.txt"),"\\s",perl=TRUE)[[1]]);
 
-# cat(sprintf("length: %s\n",length(text)));
-# cat(sprintf("max length: %s\n",max(nchar(text))));
-
 wordcounts<-table(unlist(text));
 wordcounts<-cbind.data.frame(names(wordcounts),strtoi(wordcounts));
 names(wordcounts)<-c("word","count");
 wordcounts<-wordcounts[order(-wordcounts$count),,drop=FALSE];
-wordcounts<-cbind.data.frame(wordcounts,nchar(as.vector(wordcounts$word)));
+wordsizes<-nchar(as.vector(wordcounts$word));
+wordcounts<-cbind.data.frame(wordcounts,wordsizes);
 names(wordcounts)<-c("word","count","wordsize");
+
+lettercount<-sum(wordsizes);
+maxwordsize<-max(wordsizes);
+cat(sprintf("longest word length: %s\n",maxwordsize));
+cat(sprintf("total letters: %s\n",lettercount));
+cat(sprintf("total words: %s\n",nrow(wordcounts)));
+cat(sprintf("longest word: %s\n",filter(wordcounts,wordsize==maxwordsize)[1,][["word"]]));
 
 print(wordcounts);
