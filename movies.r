a31c5afd5cab7c5efb246472f12778b06f3c51eb
risonakamo <hmykanae@gmail.com>
2018-04-05 14:56:20 -0400

    stop ggplot warnings moved things

--- movies.r
+++ movies.r
@@ -12,7 +12,7 @@ bars<-function(alldata)
     # print(relevantdata);
 
     ggplot(data=relevantdata,aes(x=clean_test,y=n,fill=c("#332d44","#854d81","#506684","#7083a4","#397694"),width=1))+geom_bar(stat="identity")+labs(y="amount of movies",x="status",title="amounts of movies with certain statuses")+theme(panel.background=element_rect(fill="white",colour="white"),panel.grid.major.y=element_line(colour="#26272a",size=.08),axis.line.x=element_line(colour="black"),axis.ticks=element_blank(),panel.grid.major.x=element_blank(),plot.title=element_text(hjust=.5))+scale_fill_manual(guide=FALSE,values=c("#332d44","#854d81","#506684","#7083a4","#397694"));
-    ggsave("plot1.png");
+    suppressMessages(ggsave("plot1.png"));
 }
 
 violin<-function(alldata)
@@ -27,7 +27,7 @@ violin<-function(alldata)
     # print(relevantdata);
 
     ggplot(data=relevantdata,aes(binary,code))+geom_violin(color="#f5b0be",fill="#f5b0be")+labs(y="year",x="pass or fail",title="movies that passed or failed vs time")+theme(panel.background=element_rect(fill="white",colour="white"),panel.grid.major.y=element_line(colour="#26272a",size=.08),axis.line.x=element_line(colour="black"),axis.ticks=element_blank(),panel.grid.major.x=element_blank(),plot.title=element_text(hjust=.5));
-    ggsave("plot2.png");
+    suppressMessages(ggsave("plot2.png"));
 }
 
 glowworms<-function(alldata)
@@ -40,7 +40,7 @@ glowworms<-function(alldata)
     # print(rdata);
 
     ggplot(data=rdata,aes(x=code,y=budget_2013.,color=binary,fill=binary))+geom_smooth()+labs(y="average budget ($)",x="year",title="average budget and number of movies released per year",colour="movie status",fill="nope")+theme(panel.background=element_rect(fill="white",colour="white"),panel.grid.major.y=element_line(colour="#26272a",size=.08),axis.line.x=element_line(colour="black"),axis.ticks=element_blank(),panel.grid.major.x=element_blank(),plot.title=element_text(hjust=.5))+scale_colour_manual(values=c("#928b63","#504c70"))+scale_fill_manual(values=c("#928b63","#504c70"),guide="none")+scale_y_continuous(breaks=seq(0,80000000,20000000),labels=comma);
-    ggsave("plot3.png");
+    suppressMessages(ggsave("plot3.png"));
 }
 
 scattuh<-function(alldata)
@@ -51,8 +51,8 @@ scattuh<-function(alldata)
     # print(rdata);
     # print(alldata);
 
-    ggplot(data=rdata,aes(x=budget_2013.,y=domgross_2013.,colour=clean_test))+geom_point()+labs(y="gross ($)",x="budget ($)",title="movies budget vs domestic gross and their statuses",colour="movie status")+theme(panel.background=element_rect(fill="white",colour="white"),panel.grid.major.y=element_line(colour="#26272a",size=.08),axis.line.x=element_line(colour="black"),axis.ticks=element_blank(),panel.grid.major.x=element_blank(),plot.title=element_text(hjust=.5))+scale_y_continuous(breaks=seq(0,1800000000,187500000),labels=comma)+scale_x_continuous(breaks=seq(0,400000000,100000000),labels=comma)+scale_colour_manual(values=c("#928b63","#504c70","#798d96","#ad685e","#ce44a5"));
-    ggsave("plot4.png");
+    ggplot(data=rdata,aes(x=budget_2013.,y=domgross_2013.,colour=clean_test))+geom_point(na.rm=TRUE)+labs(y="gross ($)",x="budget ($)",title="movies budget vs domestic gross and their statuses",colour="movie status")+theme(panel.background=element_rect(fill="white",colour="white"),panel.grid.major.y=element_line(colour="#26272a",size=.08),axis.line.x=element_line(colour="black"),axis.ticks=element_blank(),panel.grid.major.x=element_blank(),plot.title=element_text(hjust=.5))+scale_y_continuous(breaks=seq(0,1800000000,187500000),labels=comma)+scale_x_continuous(breaks=seq(0,400000000,100000000),labels=comma)+scale_colour_manual(values=c("#928b63","#504c70","#798d96","#ad685e","#ce44a5"));
+    suppressMessages(ggsave("plot4.png"));
 }
 
 makePlots<-function()
--- readme.txt
+++ readme.txt
@@ -1,3 +1,8 @@
 433 scripting languages proj 3
 name: khang ngo
 email: khang4@umbc.edu
+
+-document takes kind of long on the longer txt files (on the gl machine)
+    -so corpus will take a little long, but the times i tested it on the given files, it
+     did eventually work (so if it gets killed maybe try one more time?)
+-the word count numbers are just slightly off and i couldnt figure out why
